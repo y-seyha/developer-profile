0c8a3df9bc6b5d405b20c2e71d5d28c7359d3098
Yoeun Seyha <seyha.yoeun@tsc.gov.kh>
2026-09-19 00:45:51 +0700

    feat:create dev profile v2, set up tailwindcss and shadcn

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import { fileURLToPath } from "url";
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
